e96fa9a2d432e92bfdf2ea5c1724344bb4c5a378
Frank Netscher <fnetscher@tacc.utexas.edu>
2023-06-08 11:52:16 -0500

    Adding test for topnav navigation to Public Data.

--- tests/publicData/publicData.spec.js
+++ tests/publicData/publicData.spec.js
@@ -1,4 +1,7 @@
-test('test navigation to public data page', async ({ page, portal, environment }) => {
+import { expect, base } from '@playwright/test';
+import { test } from '../../fixtures/baseFixture'
+
+test('test sidenav navigation to public data page', async ({ page, portal, environment }) => {
 
     const url = `https://${environment === 'prod' ? '' : `${environment}.`}${portal}.tacc.utexas.edu`;
     await page.goto(url);
@@ -9,4 +12,14 @@ test('test navigation to public data page', async ({ page, portal, environment }
     const heading = page.getByRole('heading', {level: 2});
     await expect(heading).toHaveText('Public Data');
     
+  });
+
+  test('test topnav navigation to public data page', async ({ page, portal, environment }) => {
+    
+    const url = `https://${environment === 'prod' ? '' : `${environment}.`}${portal}.tacc.utexas.edu`;
+    await page.goto(url);
+    await page.getByRole('link', { name: 'Public Data' }).click();
+    
+    const heading = page.getByRole('heading', {level: 2});
+    await expect(heading).toHaveText('Public Data');
   });
