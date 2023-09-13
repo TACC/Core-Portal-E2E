e16e44a1ba984139c58735f68eb84372b3f5fd3b
Juan Martinez <jmartinez@tacc.utexas.edu>
2023-09-13 15:47:53 -0500

    Create folder user case.

--- tests/data-files/data-files-my-data.spec.js
+++ tests/data-files/data-files-my-data.spec.js
@@ -40,3 +40,28 @@ test('test navigation to my data frontera', async ({ page, portal, environment }
   const heading = page.getByRole('heading', {level: 2});
   await expect(heading).toHaveText("My Data (Frontera)");
 });
+
+test('test create new folder', async ({ page, portal, environment }) => {
+
+  // Navigate to Create Folder dialog
+  const url = `https://${environment === 'prod' ? '' : `${environment}.`}${portal}.tacc.utexas.edu`;
+  await page.goto(url);
+  await page.locator('#navbarDropdown').click();
+  await page.getByRole('link', { name: 'Dashboard' }).click();
+  await page.getByRole('link', { name: 'Data Files' }).click();
+  await page.getByRole('button', { name: '+ Add'}).click();
+  await page.locator('button:has-text(" Folder")').click();
+
+  // Test folder creation
+  const folderName = page.locator('[name="dirname"]');
+  await folderName.fill('qa-test-folder-1');
+  await page.locator('button:has-text("Create Folder")').click();
+
+  await expect(page.locator('div.tr:has-text("qa-test-folder-1")')).toBeVisible();
+
+  // Delete test folder
+  await page.locator('div.tr:has-text("qa-test-folder-1")').click();
+  await page.locator('button:has-text("Trash")').click();
+
+
+});
