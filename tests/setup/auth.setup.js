04875baeb11b3d083ce8e7602db003b42309f009
shayanaijaz <shayanaijaz@gmail.com>
2023-10-12 13:24:11 -0500

    quick fix relating to new tapis login page

--- tests/setup/auth.setup.js
+++ tests/setup/auth.setup.js
@@ -11,7 +11,7 @@ setup('authenticate', async ({ page, portal, environment }) => {
   await page.getByLabel('Password').click();
   await page.getByLabel('Password').fill(process.env.PASSWORD);
   await page.getByRole('button', { name: 'Login' }).click();
-  await page.getByRole('button', { name: 'Submit' }).click();
+  await page.getByRole('button', { name: 'Connect' }).click();
 
   await page.context().storageState({path: authFile})
 });
