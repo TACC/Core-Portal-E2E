01e768b3bbd9b5b8b5d5d777a5894f3ffcc5da6d
shayanaijaz <shayanaijaz@gmail.com>
2023-10-17 10:45:51 -0500

    update for new tapis login page

--- tests/setup/auth.setup.js
+++ tests/setup/auth.setup.js
@@ -6,12 +6,12 @@ setup('authenticate', async ({ page, portal, environment }) => {
   const url = `https://${environment === 'prod' ? '' : `${environment}.`}${portal}.tacc.utexas.edu`;
   await page.goto(url);
   await page.getByRole('link', { name: 'Log in' }).click();
-  await page.getByPlaceholder('Enter Username').click();
-  await page.getByPlaceholder('Enter Username').fill(process.env.USERNAME);
-  await page.getByPlaceholder('Enter Password').click();
-  await page.getByPlaceholder('Enter Password').fill(process.env.PASSWORD);
+  await page.getByLabel('Username').click();
+  await page.getByLabel('Username').fill(process.env.USERNAME);
+  await page.getByLabel('Password').click();
+  await page.getByLabel('Password').fill(process.env.PASSWORD);
   await page.getByRole('button', { name: 'Login' }).click();
-  await page.getByRole('button', { name: 'Submit' }).click();
+  await page.getByRole('button', { name: 'Connect' }).click();
 
   await page.context().storageState({path: authFile})
 });
