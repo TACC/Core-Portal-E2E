b64f8635753175fb819c81ae8ff4e6f17fee1358
Sarah Gray <sgray@tacc.utexas.edu>
2023-12-18 12:03:15 -0600

    updating tests to new way of doing the url

--- tests/data-files/data-files-my-data-operations.spec.js
+++ tests/data-files/data-files-my-data-operations.spec.js
@@ -2,11 +2,8 @@ import { expect, base } from '@playwright/test';
 import { test } from '../../fixtures/baseFixture';
 
 test.describe('Data Files My Data Work Operations tests', () => {
-    // test.beforeEach(async ({ page, portal, environment, baseURL }) => {
-    //     await page.goto(baseURL);
-    test.beforeEach(async ({ page, portal, environment }) => {
-        const url = `https://${environment === 'prod' ? '' : `${environment}.`}${portal}.tacc.utexas.edu`;
-        await page.goto(url);
+    test.beforeEach(async ({ page, portal, environment, baseURL }) => {
+        await page.goto(baseURL);
         await page.locator('#navbarDropdown').click();
         await page.getByRole('link', { name: 'My Dashboard' }).click();
         await page.getByRole('link', { name: 'Data Files', exact: true }).click();
