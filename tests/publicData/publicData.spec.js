1d24b67e1cc54734f64574ea8e6ee0432917dbba
Frank Netscher <fnetscher@tacc.utexas.edu>
2024-01-19 13:46:57 -0600

    Public Data gets skipped if the portal isn't CEP

--- tests/publicData/publicData.spec.js
+++ tests/publicData/publicData.spec.js
@@ -1,10 +1,10 @@
 import { expect, base } from '@playwright/test';
-import { test } from '../../fixtures/baseFixture'
- 
-
+import { test } from '../../fixtures/baseFixture.js';
+import { NGINX_SERVER_NAME } from '../../settings/custom_portal_settings.json';
 
 test.describe('Public Data Tests', () => {
-  
+  test.skip(NGINX_SERVER_NAME != 'cep.tacc.utexas.edu', 'Public Data not on portal, test skipped');
+
   test.beforeEach(async ({ page, portal, environment, baseURL }) => {
     await page.goto(baseURL);
   })
