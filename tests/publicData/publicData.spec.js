7c1a6e4056987ac43004015525e9ea45daa38efc
Frank Netscher <fnetscher@tacc.utexas.edu>
2024-09-19 15:55:14 -0500

    Added a check to see if the portal will have Public Data.

--- tests/publicData/publicData.spec.js
+++ tests/publicData/publicData.spec.js
@@ -2,15 +2,24 @@ import { expect, base } from '@playwright/test';
 import { test } from '../../fixtures/baseFixture';
 import { PORTAL_DATAFILES_STORAGE_SYSTEMS } from '../../settings/custom_portal_settings.json';
 
-test.describe('Public Data Tests', () => {
-  test.skip(PORTAL_DATAFILES_STORAGE_SYSTEMS != 'Public Data', 'Public Data not on portal, test skipped');
+let publicDataEntry = false;
+for (const entry of PORTAL_DATAFILES_STORAGE_SYSTEMS) {
+  if (entry.name === "Public Data") {
+    publicDataEntry = true;
+    break;
+  }
+}
+
+test.describe('Public Data Tests', async () => {
+  
+  test.skip(publicDataEntry === false, 'Public Data not on portal, test skipped');
 
   test.beforeEach(async ({ page, portal, environment, baseURL }) => {
     await page.goto(baseURL);
   })
     
   test('test topnav navigation to public data page', async ({ page, portal, environment, baseURL }) => {
-      
+
       await page.getByRole('link', { name: 'Public Data' }).click();
       
       const heading = page.getByRole('heading', {level: 2});
